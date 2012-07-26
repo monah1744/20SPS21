95c2e72623dde82635c919138bc5afab8523fb43
monah1744 <monah1744@gmail.com>
2012-07-26 14:27:49 +0300

    temp

---
+++ hq_for_jule/src/main.cc
@@ -0,0 +1,31 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * main.cc
+shell-init: ошибка получения текущей директории: getcwd: cannot access parent directories: Нет такого файла или каталога
+chdir: ошибка получения текущей директории: getcwd: cannot access parent directories: Нет такого файла или каталога
+ * Copyright (C) monah1744 2012 <monah1744@gmail.com>
+ * 
+HQ_For_Jule is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * HQ_For_Jule is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <stdlib.h>
+#include <locale>
+
+using namespace std;
+int main()
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
