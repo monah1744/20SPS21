1505ecdf888a4e01d7a79c593f140d2a71becac7
monah1744 <monah1744@gmail.com>
2012-10-05 00:04:41 +0300

    modified:   array/CMakeLists.txt.user 	modified:   array/main.cpp 	modified:   array/my_array.h

--- array/main.cpp
+++ array/main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include "my_array.h"
 
+typedef double Type;
 using namespace std;
 int main ()
-{}
+{
+    int M=7;
+    srand(time(NULL));
+    cout <<"начало  ---\n";
+    Type *a = new Type[M];
+
+    for (int i=0;i<M;i++)
+        a[i]=Type(rand()%100);
+    cout<<endl<<"---continue---"<<endl;
+    int Mexit=0;
+    do
+    {
+        cout<<"Выберите Действие :\n1 - Увеличение массива,\n2 - Уменьшение массива,\n3 - Вывод массива\n0 - Выход\n";
+        cin>>Mexit;
+        switch (Mexit)
+        {
+        case (1) : {
+            int Pos;
+            Type Num;
+            cout<<"Введите значение вставляемой переменной : ";
+            cin>>Num;
+            cout<<"Введите позицию вставляемой переменной (Нулевой елемент является первым !!!) : ";
+            cin>>Pos;
+            a=ArrayInc(a,&M,Pos,Num);
+            break;
+        }
+        case (2) : {
+            int Pos;
+            cout<<"Введите позицию удаляемой переменной (Нулевой елемент является первым !!!) : ";
+            cin>>Pos;
+            a=ArrayDec(a,&M,Pos);
+            break;
+        }
+        case (3) :
+        {
+            ArrayShow(a,&M);
+            break;
+        }
+        case (0) :
+        {
+            exit(0);
+        }
+        default :
+        {
+            cout<<"Значение не определено. Повторите еще раз...\n";
+        }
+        }
+    }
+    while(Mexit!=0);
+}
--- array/my_array.h
+++ array/my_array.h
@@ -1,26 +1,38 @@
 #ifndef MY_ARRAY_H
 #define MY_ARRAY_H
 
-void ArrayInc ();
-void ArrayDec ();
-void ArrayShow ();
-
-
 //======================================================================
 
-void ArrayInc ()
+template <typename Tarr, typename Def> Tarr* ArrayInc (Tarr *arr, int* Index, int Pos, Def Num)
 {
-
+    *Index=*Index+1;
+    Tarr *arr1 = new Tarr[*Index];
+    for (int i=0;i<*Index-1;i++)
+        *(arr1+i)=*(arr+i);
+    delete [] arr;
+    for (int i=*Index-1;i>Pos;i--)
+        *(arr1+i)=*(arr1+i-1);
+    *(arr1+Pos)=Num;
+    return arr1;
 }
 
-void ArrayDec ()
+template <typename Tarr> Tarr* ArrayDec (Tarr *arr, int* Index, int Pos)
 {
+    for (int i=Pos;i<*Index-1;i++)
+        *(arr+i)=*(arr+i+1);
+    *Index=*Index-1;
 
+    Tarr *arr1 = new Tarr[*Index];
+    for (int i=0;i<*Index;i++)
+        *(arr1+i)=*(arr+i);
+    delete [] arr;
+    return arr1;
 }
 
-void ArrayShow ()
+template <typename Tarr> void ArrayShow (Tarr *arr, int* Index)
 {
-
+    for (int i=0;i<*Index;i++)
+        std::cout<<*(arr+i)<<"\t";
 }
 
 #endif // MY_ARRAY_H
