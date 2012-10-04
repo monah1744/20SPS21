6a5aaa1fa2d51fe99a331a7236d067845c8d46c9
monah1744 <monah1744@gmail.com>
2012-10-04 07:23:26 +0300

    new file:   array/CMakeLists.txt 	new file:   array/CMakeLists.txt.user 	new file:   array/main.cpp 	new file:   array/my_array.h

---
+++ array/my_array.h
@@ -0,0 +1,26 @@
+#ifndef MY_ARRAY_H
+#define MY_ARRAY_H
+
+void ArrayInc ();
+void ArrayDec ();
+void ArrayShow ();
+
+
+//======================================================================
+
+void ArrayInc ()
+{
+
+}
+
+void ArrayDec ()
+{
+
+}
+
+void ArrayShow ()
+{
+
+}
+
+#endif // MY_ARRAY_H
